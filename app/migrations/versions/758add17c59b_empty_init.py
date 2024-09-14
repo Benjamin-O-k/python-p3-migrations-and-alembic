6c9f92385086235ea59ce9ab86a4de448942caf1
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-09-14 21:36:54 +0300

    passed

---
+++ app/migrations/versions/758add17c59b_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 758add17c59b
+Revises: 
+Create Date: 2024-09-14 21:30:07.269061
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '758add17c59b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
